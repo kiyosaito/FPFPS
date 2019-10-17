c118eb6c262c22912a6ac459a87be3180c381848
Adam Rudas <jotemair@gmail.com>
2019-10-17 16:02:47 +1100

    Input manager singleton basics

--- Assets/Demo/Scripts/InputManager.cs
+++ Assets/Demo/Scripts/InputManager.cs
@@ -2,5 +2,29 @@
 
 public class InputManager
 {
-    
+    #region Singleton
+
+    private readonly object instanceGetLock = new object();
+
+    private static InputManager _instance = null;
+
+    public InputManager Instance
+    {
+        get
+        {
+            lock (instanceGetLock)
+            {
+                if (null == _instance)
+                {
+                    _instance = new InputManager();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    private InputManager() { }
+
+    #endregion
 }
