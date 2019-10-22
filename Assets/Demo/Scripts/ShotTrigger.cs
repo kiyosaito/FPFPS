4c53dc3adda169a6a4a01ebc9cd102f7378fcd83
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-10-22 15:48:24 +1100

    More demo

---
+++ Assets/Demo/Scripts/ShotTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class ShotTrigger : MonoBehaviour,Target
+{
+    public string hitTag;
+    public UnityEvent onEnter, onStay, onExit;
+    public bool isActive = false;
+
+    private void Reset()
+    {
+        Collider col = GetComponent<Collider>();
+        if (col)
+        {
+            col.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((other.tag == hitTag || hitTag == "") && (isActive))
+        {
+            onEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if ((other.tag == hitTag || hitTag == "") && (isActive))
+        {
+            onStay.Invoke(); // Note (Manny): This was onEnter
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((other.tag == hitTag || hitTag == "") && (isActive))
+        {
+            onExit.Invoke(); // Note (Manny): This was also onEnter
+        }
+    }
+
+    public void GetShot(bool charged)
+    {
+        isActive = true;
+
+    }
+}
