28764b52e2c81414b2b8f46d4ea6a9e7f83839f0
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-10-23 11:57:28 +1100

    checkpoint and out of bounds

---
+++ Assets/Martin/Scripts/CheckPoint/CheckPointTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointTrigger : CheckPointManager
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            checkpoint = gameObject.transform.position;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Martin/Scripts/CheckPoint/OutOfBounds.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private Player playerScript;
+    private CheckPointManager checkPoint;
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<Player>();
+        checkPoint = FindObjectOfType<CheckPointManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DisablePlayer();
+            Invoke("RespawnPlayer",0.5f);
+            Invoke("EnablePlayer", 0.5f);
+        }
+    }
+    void DisablePlayer()
+    {
+        playerScript.enabled = false;
+    }
+    void RespawnPlayer()
+    {
+        player.transform.position = checkPoint.transform.position;
+    }
+    void EnablePlayer()
+    {
+        playerScript.enabled = true;
+    }
+}
