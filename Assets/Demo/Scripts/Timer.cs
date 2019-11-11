347956b1063eae0113d73c13aeb0eb4cb45152cf
Adam Rudas <jotemair@gmail.com>
2019-11-11 16:09:21 +1100

    Added debug timer

---
+++ Assets/Demo/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Text timer = null;
+
+    private float time = 0f;
+
+    private bool running = true;
+
+    private void Update()
+    {
+        if (running)
+        {
+            time += Time.deltaTime;
+
+            timer.text = ((((((int)time) / 60)) < 10) ? "0" : "") + (((int)time) / 60).ToString() + ":" + (((((int)time) % 60) < 10) ? "0" : "")
+                + (((int)time) % 60).ToString() + "." + (((((int)(time * 100f)) % 100) < 10) ? "0" : "" ) + (((int)(time * 100f)) % 100).ToString();
+        }
+    }
+
+    public void Stop()
+    {
+        running = false;
+    }
+}
