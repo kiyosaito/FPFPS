5f3189eb77cae28216ee6ba33dff7888fb765fa5
kiyosaito <48036649+kiyosaito@users.noreply.github.com>
2019-11-24 06:55:26 +1100

    pause menu
    almost complete

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject pause;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        pause = GameObject.Find("Hologram");
+
+    }
+    public void Pausing()
+    {
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pause.gameObject.SetActive(true);
+    }
+    public void Unpausing()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pause.gameObject.SetActive(false);
+    }
+    public void QTM()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
