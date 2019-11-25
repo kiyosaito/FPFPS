8e448096d849e87eeb1641b896b5002af0636591
Adam Rudas <jotemair@gmail.com>
2019-11-25 13:06:45 +1100

    Updated timer options in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -26,11 +26,13 @@ public class GameManager : UnitySingleton<GameManager>
 
     private TimerDifficultySetting _timerDifficultySetting = TimerDifficultySetting.Normal;
 
-    private bool _showTotalTime = true;
+    private bool _showTimer = true;
 
     private bool _showSplits = false;
 
     private bool _showSegmentTime = false;
+
+    private bool _showTotalTimeComparison = false;
 
     private GameScene _levelReached = GameScene.MainMenu;
 
@@ -62,19 +64,28 @@ public class GameManager : UnitySingleton<GameManager>
         Easy,   // Pianissimo   // There is no remaining time
     }
 
-    public bool ShowTotalTime
+    public bool ShowTimer
     {
-        get { return _showTotalTime; }
+        get { return _showTimer; }
+        set { _showTimer = value; }
     }
 
     public bool ShowSplits
     {
         get { return _showSplits; }
+        set { _showSplits = value; }
     }
 
     public bool ShowSegmentTime
     {
         get { return _showSegmentTime; }
+        set { _showSegmentTime = value; }
+    }
+
+    public bool ShowTotalTimeComparison
+    {
+        get { return _showTotalTimeComparison; }
+        set { _showTotalTimeComparison = value; }
     }
 
     public GameScene CurrentLevel
